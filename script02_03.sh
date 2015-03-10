b854e5bfd541d3fadbb7ae3d697a977951c29a66
Dante™ <dante.toscano1997@gmail.com>
2015-03-10 19:24:44 +0100

    Update script02_03.sh

--- script02_03.sh
+++ script02_03.sh
@@ -4,3 +4,11 @@
 # 1) Nota il risultato del comando cat accounts.txt
 #
 # Questo crea, nella directory corrente, una directory per ogni account riportato nel file accounts.txt
+
+x=`cat accounts.txt`
+
+#Ciclo necessario alla creazine delle directory, il cui nome è salvato nella variabile "nomiDirectory"
+for y in $x
+do
+	mkdir $y
+done
