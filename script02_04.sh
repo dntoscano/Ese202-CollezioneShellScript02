c12b4bbd9fada6f36e0a2e5d07fa9183eed53b78
Dante™ <dante.toscano1997@gmail.com>
2015-03-10 19:26:19 +0100

    Update script02_04.sh

--- script02_04.sh
+++ script02_04.sh
@@ -3,3 +3,11 @@
 #
 # Questo script cancella tutte le sottodirectory (e il loro eventuale contentuo) della directory corrente,
 # il cui nome è contenuto nel file accounts.txt
+
+x=`cat accounts.txt`
+
+#Ciclo necessario alla creazine delle directory, il cui nome è salvato nella variabile "nomiDirectory"
+for y in $x
+do
+	rm -r $y
+done
